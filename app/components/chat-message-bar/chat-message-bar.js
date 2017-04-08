69446a84d34a9813eb5536b40916f8aade489bab
Roy S <roy.straub@sogeti.com>
2017-04-08 16:37:18 +0200

    and other files too

--- app/components/chat-message-bar/chat-message-bar.js
+++ app/components/chat-message-bar/chat-message-bar.js
@@ -1,32 +1,36 @@
 angular.module('app')
-.component('chatMessageBar',{
-    controller: chatMessageBarController,
-    controllerAs: 'vm',
-    templateUrl: 'components/chat-message-bar/chat-message-bar.html',
-    bindings: {
-        currentRoom: '<',
-        self: '<'
-    }
-});
+    .component('chatMessageBar', {
+        controller: chatMessageBarController,
+        controllerAs: 'vm',
+        templateUrl: 'components/chat-message-bar/chat-message-bar.html',
+        bindings: {
+            currentRoom: '<',
+            self: '<'
+        }
+    });
 
-chatMessageBarController.$inject = ['$rootScope','$log'];
+chatMessageBarController.$inject = ['$rootScope', '$log', 'EmojiConverter'];
 
-function chatMessageBarController($rootScope,$log){
+function chatMessageBarController($rootScope, $log, emojiConverter) {
     var vm = this;
     vm.message = '';
     vm.sendMessage = sendMessage;
 
     var socket = null;
 
-    function sendMessage(){
-        $log.debug('You said:',vm.message);
+    function sendMessage() {
+        $log.debug('You said:', vm.message);
         var messageData = {
             username: vm.self.username,
             email: vm.self.email,
             content: vm.message,
             room: vm.currentRoom
         };
-        $rootScope.$broadcast('socket:message',messageData);
+        var content = vm.message;
+        
+        messageData.content = emojiConverter.getEmojiString(vm.message);
+
+        $rootScope.$broadcast('socket:message', messageData);
         vm.message = '';
     }
 }
